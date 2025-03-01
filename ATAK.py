0ddd4ead9965f9922d562bfac554de132f623f85
casper0169 <ihackyouwin@gmail.com>
2025-03-01 10:43:23 +0100

    Update ATAK.py

--- ATAK.py
+++ ATAK.py
@@ -5,19 +5,27 @@ import time  # Importado para manejar el intervalo entre comandos
 
 def mostrar_menu_principal():
     os.system('clear')
-    print("|============================================|")
+    print("|--------------------------------------------|")
     print("|       MENÚ PRINCIPAL - SERVIDOR TAK        |")
-    print("|============================================|")
+    print("|--------------------------------------------|")
     print("| 1. Instalación del Servidor TAK            |")
+    print("|--------------------------------------------|")
     print("| 2. Definir dirección IPv4 privada estática |")
+    print("|--------------------------------------------|")
     print("| 3. Definir los puertos del Firewall        |")
+    print("|--------------------------------------------|")
     print("| 4. Mostrar certificados existentes         |")
+    print("|--------------------------------------------|")
     print("| 5. Crear certificados                      |")
+    print("|--------------------------------------------|")
     print("| 6. Eliminar certificados                   |")
+    print("|--------------------------------------------|")
     print("| 7. Gestión de usuarios en grupos           |")
+    print("|--------------------------------------------|")
     print("| 8. URLs de interés                         |")
+    print("|--------------------------------------------|")
     print("| 9. Salir                                   |")
-    print("|============================================|")
+    print("|--------------------------------------------|")
 
 def ejecutar_comando(comando):
     print(f"Ejecutando: {comando}")
@@ -39,7 +47,7 @@ def obtener_ip_local():
     return ip_local
 
 def instalar_tak():
-    print("Instalando Servidor TAK automáticamente...")
+    print("INSTALANDO EL SERVIDOR TAK AUTOMÁTICAMENTE ...")
     ejecutar_comando("sudo apt update && apt full-upgrade -y")
     ejecutar_comando("echo '* soft nofile 32768\n* hard nofile 32768' | sudo tee -a /etc/security/limits.conf > /dev/null")
     ejecutar_comando("sudo mkdir -p /etc/apt/keyrings")
@@ -51,13 +59,13 @@ def instalar_tak():
     ejecutar_comando("sudo update-alternatives --config java")
     ejecutar_comando("sudo apt install -y maven gradle")
     time.sleep(2)
-    print("Por favor, ingresa el nombre del archivo instalable del servidor TAK (por ejemplo, 'takserver-installer.deb'): ")
+    print("POR FAVOR, INGRESA EL NOMBRE DEL ARCHIVO INSTALABLE DEL SERVIDOR TAK (por ejemplo, 'takserver-installer.deb'): ")
     ejecutable_tak = input("Nombre del archivo instalable: ").strip()
     if os.path.exists(f"./{ejecutable_tak}"):
         print(f"Instalando el archivo {ejecutable_tak}...")
         ejecutar_comando(f"sudo apt install ./{ejecutable_tak}")
     else:
-        print(f"Error: El archivo {ejecutable_tak} no se encuentra en el directorio.")
+        print(f"Error: EL ARCHIVO {ejecutable_tak} NO SE ENCUENTRA EN EL DIRECTORIO.")
         return
     ejecutar_comando("sudo systemctl daemon-reload")
     ejecutar_comando("sudo systemctl start takserver")
@@ -65,19 +73,19 @@ def instalar_tak():
     ejecutar_comando("sudo systemctl restart takserver")
     time.sleep(2)
     archivo_metadata = "/opt/tak/certs/cert-metadata.sh"
-    print(f"Editando {archivo_metadata} automáticamente...")
+    print(f"EDITANDO {archivo_metadata} AUTOMÁTICMENTE...")
     try:
         with open(archivo_metadata, 'a') as file:
-            file.write("\n# Configuración automática añadida por el script\n")
+            file.write("\n# CONFIGURACIÓN AUTOMÁTICA AÑADIDA POR EL script\n")
     except Exception as e:
         print(f"Error al editar {archivo_metadata}: {e}")
-    print(f"Abriendo {archivo_metadata} para su edición...")
+    print(f"ABRIENDO {archivo_metadata} PARA SU EDICIÓN ...")
     os.system(f"sudo nano {archivo_metadata}")
     print("\033[1m\033[32m********** ¡ENHORABUENA! INSTALACIÓN DEL SERVIDOR TAK COMPLETADO. **********\033[0m")
 
 def configurar_ipv4_estatica():
-    print("Configura la dirección IPv4 privada estática en /etc/netplan/00-installer-config.yaml.")
-    print("Ejemplo de configuración:\n"
+    print("CONFIGURACIÓN DE LA DIRECCIÓN IPv4 PRIVADA ESTÁTICA EN EL DIRECTORIO: /etc/netplan/00-installer-config.yaml.")
+    print("EJEMPLO DE CONFIGURACIÓN:\n"
           "network:\n"
           "  version: 2\n"
           "  renderer: NetworkManager\n"
@@ -89,20 +97,20 @@ def configurar_ipv4_estatica():
           "        addresses:\n"
           "          - 8.8.8.8\n"
           "          - 8.8.4.4")
-    input("Presiona INTRO para editar o ESCAPE para volver al menú principal.")
+    input("PRESIONA [INTRO] PARA EDITAR ó [ESCAPE] PARA VOLVER AL MENÚ PRINCIPAL.")
     os.system("sudo nano /etc/netplan/00-installer-config.yaml")
     comandos = ["sudo netplan apply", "sudo systemctl restart NetworkManager"]
     for comando in comandos:
         try:
             ejecutar_comando(comando)
         except subprocess.CalledProcessError as e:
-            print(f"Error al ejecutar '{comando}': {e}. Verifica la configuración e intenta nuevamente.")
+            print(f"Error al ejecutar '{comando}': {e}. VERIFICA LA INSTALACIÓN Y REPITE EL PROCESO NUEVAMENTE.")
             break
         time.sleep(2)
     print("\033[1m\033[32m********** ¡ENHORABUENA! CONFIGURACIÓN DE RED COMPLETADA. **********\033[0m")
 
 def configurar_firewall():
-    print("Configurando el Firewall...")
+    print("CONFIGURANDO EL FIREWALL DEL SERVIDOR TAK ...")
     ejecutar_comando("sudo apt-get install -y ufw")
     ejecutar_comando("sudo ufw enable")
     ejecutar_comando("sudo ufw allow 8089/tcp")
@@ -115,11 +123,11 @@ def mostrar_certificados():
     directorio_certificados = "/opt/tak/certs/files"
     
     if not os.path.exists(directorio_certificados):
-        print("No hay certificados.")
-        input("Presiona INTRO para volver al menú principal...")
+        print("\033[1m\033[32m********** ¡ NO HAY CERTIFICADOS EXISTENTES ! **********\033[0m")
+        input("PRESIONA [INTRO] PARA VOLVER AL MENÚ PRINCIPAL ...")
         return  # Regresar al menú principal
     
-    print("Mostrando todo el contenido en /opt/tak/certs/files:")
+    print("MOSTRANDO TODO EL CONTENIDO DEL DIRECTORIO: /opt/tak/certs/files:")
     # Listar todos los archivos en la carpeta
     certificados = os.listdir(directorio_certificados)
     
@@ -128,33 +136,38 @@ def mostrar_certificados():
         for certificado in certificados:
             print(certificado)
     else:
-        print("No hay certificados existentes.")
+        print("¡ NO HAY CERTIFICADOS EXISTENTES !")
     
-    input("Presiona INTRO para volver al menú principal...")
+    input("PRESIONA [INTRO] PARA VOLVER AL MENÚ PRINCIPAL ...")
 
 def crear_certificados():
-    print("Submenú - Crear Certificados")
-    print("1. Crear Entidad Certificadora")
-    print("2. Crear Certificados Genéricos")
-    print("3. Atrás")
-    opcion = input("Seleccione una opción: ")
+    print("|----------------------------------|")
+    print("|    SUBMEÚ - CREAR CERTIFICADOS   |")
+    print("|----------------------------------|")
+    print("| 1. Crear Entidad Certificadora   |")
+    print("|----------------------------------|")
+    print("| 2. Crear Certificados Genéricos  |")
+    print("|----------------------------------|")
+    print("| 3. Atrás                         |")
+    print("|----------------------------------|")
+    opcion = input("SELECCIONE UNA OPCIÓN: ")
     if opcion == "1":
-        print("Creando certificado de Autoridad Certificadora...")
+        print("CREANDO CERTIFICADO DE ENTIDAD CERTIFICADORA ...")
         ejecutar_comando("cd /opt/tak/certs && ./makeRootCa.sh")
-        print("Certificado de Autoridad Certificadora creado correctamente.")
+        print("\033[1m\033[32m********** ¡ CERTIFICADO DE [AUTORIDAD CERTIFICADORA] CREADO CORRECTAMENTE ! **********\033[0m")
     elif opcion == "2":
-        tipo_certificado = input("Defina el tipo de certificado (client/server): ")
-        nombres_certificados = input("Introduzca el nombre del certificado(s) separados por comas: ")
+        tipo_certificado = input("DEFINA EL TIPO DE CERTIFICADO (client/server): ")
+        nombres_certificados = input("INTRODUZCA EL NOMBRE DEL CERTIFICADO, SI SON VARIOS(s) SEPARALOS POR COMAS (,): ")
         for nombre in nombres_certificados.split(","):
             ejecutar_comando(f"cd /opt/tak/certs && ./makeCert.sh {tipo_certificado} {nombre}")
-        print("Certificados creados correctamente.")
+        print("\033[1m\033[32m********** ¡ CERTIFICADO CREADO CORRECTAMENTE ! **********\033[0m")
     elif opcion == "3":
         return
     else:
-        print("Opción inválida.")
+        print("\033[1m\033[32m********** ¡ OPCIÓN INVÁLIDA ! **********\033[0m")
 
 def eliminar_certificados():
-    certificado = input("Indique el nombre del certificado a eliminar (sin extensión): ").strip()
+    certificado = input("INDIQUE EL NOMBRE DEL [CERTIFICADO] QUE DESEA ELIMINAR (sin extensión): ").strip()
     # Definir las extensiones a eliminar
     extensiones = ['.csr', '.jks', '.key', '.p12', '.pem', '-trusted.pem']
     
@@ -166,66 +179,74 @@ def eliminar_certificados():
             archivos_existentes.append(archivo)
     
     if archivos_existentes:
-        print("Certificados encontrados. Procediendo a eliminar...")
+        print("CERTIFICADO ENCONTADO, ELIMINANDO ...")
         for archivo in archivos_existentes:
-            print(f"Eliminando archivo: {archivo}")
+            print(f"ELIMINANDO: {archivo}")
             os.remove(archivo)
-        print("Certificados eliminados correctamente.")
+        print("\033[1m\033[32m********** ¡ CERTIFICADO ELIMINADO CORRECTAMENTE ! **********\033[0m")    
     else:
-        print(f"No se encontraron certificados para el nombre '{certificado}' en las extensiones especificadas.")
+        print(f"NO SE HA ENCONTRADO EL CERTIFICADO '{certificado}' EN LAS EXTENSIONES PREDETERMMINADAS.")
 
 def gestionar_usuarios():
-    print("Submenú - Gestión de Usuarios en Grupos")
-    print("1. Agregar un certificado a un grupo")
-    print("2. Desagregar un certificado de un grupo")
-    print("3. Atrás")
-    opcion = input("Seleccione una opción: ")
+    print("|--------------------------------------------|")
+    print("|  SUBMEÚ - GESTIÓN DE USUARIOS EN GRUPOS    |")
+    print("|--------------------------------------------|")
+    print("| 1. Agregar un certificado a un grupo       |")
+    print("|--------------------------------------------|")
+    print("| 2. Desagregar un certificado de un grupo   |")
+    print("|--------------------------------------------|")
+    print("| 3. Atrás                                   |")
+    print("|--------------------------------------------|")
+    opcion = input("SELECCIONE UNA OPCIÓN: ")
     if opcion == "1":
-        certificado = input("Ingrese el nombre del certificado: ")
+        certificado = input("INGRESE EL NOMBRE DEL CERTIFICADO: ")
         if os.path.isfile(f"/opt/tak/certs/files/{certificado}.pem"):
-            print("Certificado encontrado.")
-            print("1. Grupo de Administración")
-            print("2. Otros grupos")
-            subopcion = input("Seleccione una opción: ")
+            print("\033[1m\033[32m********** ¡ CERTIFICADO ENCONTRADO ! **********\033[0m")
+            print("|----------------------------|")
+            print("| 1. Grupo de Administración |")
+            print("|----------------------------|")
+            print("| 2. Otros grupos            |")
+            print("|----------------------------|")
+            subopcion = input("SELECCIONE UNA OPCIÓN: ")
             if subopcion == "1":
                 ejecutar_comando(f"java -jar /opt/tak/utils/UserManager.jar certmod -A /opt/tak/certs/files/{certificado}.pem")
-                print("Certificado agregado al grupo de Administración.")
+                print("\033[1m\033[32m********** ¡ [CERTIFICADO] AGREGADO AL [GRUPO] DE ADMINISTRACIÓN ! **********\033[0m")
             elif subopcion == "2":
-                tipo_grupo = input("Indique el tipo de grupo (Entrada/Salida/Ambas): ")
+                tipo_grupo = input("INDIQUE EL TIPO DE [GRUPO]: (Entrada/Salida/Ambas): ")
                 if tipo_grupo == "Entrada":
-                    grupo = input("Indique el nombre del grupo: ")
+                    grupo = input("INDIQUE EL NOMBRE DEL [GRUPO]: ")
                     ejecutar_comando(f"java -jar /opt/tak/utils/UserManager.jar certmod -ig{grupo} /opt/tak/certs/files/{certificado}.pem")
                 elif tipo_grupo == "Salida":
-                    grupo = input("Indique el nombre del grupo: ")
+                    grupo = input("INDIQUE EL NOMBRE DEL [GRUPO]: ")
                     ejecutar_comando(f"java -jar /opt/tak/utils/UserManager.jar certmod -og{grupo} /opt/tak/certs/files/{certificado}.pem")
                 elif tipo_grupo == "Ambas":
-                    grupo = input("Indique el nombre del grupo: ")
+                    grupo = input("INDIQUE EL NOMBRE DEL [GRUPO]: ")
                     ejecutar_comando(f"java -jar /opt/tak/utils/UserManager.jar certmod -g{grupo} /opt/tak/certs/files/{certificado}.pem")
             else:
-                print("Opción inválida.")
+                print("\033[1m\033[32m********** ¡ OPCIÓN INVÁLIDA ! **********\033[0m")
         else:
-            print("Certificado no encontrado.")
+            print("\033[1m\033[32m********** ¡ [CERTIFICADO] NO ENCONTRADO ! **********\033[0m")
     elif opcion == "2":
-        certificado = input("Ingrese el nombre del certificado: ")
+        certificado = input("INGRESE EL NOMBRE DEL [CERTIFICADO]: ")
         if os.path.isfile(f"/opt/tak/certs/files/{certificado}.pem"):
             ejecutar_comando(f"java -jar /opt/tak/utils/UserManager.jar certmod -r /opt/tak/certs/files/{certificado}.pem")
-            print("Certificado eliminado del grupo.")
+            print("\033[1m\033[32m********** ¡ [CERTIFICADO] ELIMINADO DEL [GRUPO] ! **********\033[0m")
         else:
-            print("Certificado no encontrado.")
+            print("\033[1m\033[32m********** ¡ [CERTIFICADO] NO ENCONRTADO ! **********\033[0m")
     elif opcion == "3":
         return
     else:
-        print("Opción inválida.")
+        print("\033[1m\033[32m********** ¡ OPCIÓN INVÁLIDA ! **********\033[0m")
 
 def mostrar_urls():
-    print("URLs de interés:")
-    print("TAK Server Admin Tool: https://localhost:8443/Matri/metrics/index.html")
-    print("Servidor Web TAK: https://localhost:8443/webtak/index.html")
+    print("URLs DE INTERÉS:")
+    print("TAK HERRAMIENTA DE ADMINISTRACIÓN WEB: https://localhost:8443/Matri/metrics/index.html")
+    print("SERVIDOR TAK: https://localhost:8443/webtak/index.html")
 
 def main():
     while True:
         mostrar_menu_principal()
-        opcion = input("Seleccione una opción: ")
+        opcion = input("SELECCIONE UNA OPCIÓN: ")
         if opcion == "1":
             instalar_tak()
         elif opcion == "2":
@@ -243,11 +264,12 @@ def main():
         elif opcion == "8":
             mostrar_urls()
         elif opcion == "9":
-            print("Gracias, por usar nuestra aplicación. ¡Vuelve cuándo quieras!")
+            print("\033[1m\033[32m********** ¡ GRACIAS POR USAR NUESTRA APLICACIÓN ! **********\033[0m")
+            print("\033[1m\033[32m********** ¡ VUELGVE CUANDO QUIERAS ! **********\033[0m")
             break
         else:
-            print("Opción inválida.")
-        input("Presiona INTRO para continuar...")
+            print("\033[1m\033[32m********** ¡ OPCIÓN INVÁLIDA ! **********\033[0m")
+        input("PRESIONA [INTRO] PARA CONTINUAR...")
 
 if __name__ == "__main__":
     main()
